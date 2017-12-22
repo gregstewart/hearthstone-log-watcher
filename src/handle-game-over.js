52abe41a5d4218d43d0b398e33b47a322173f36a
Greg Stewart <gregs@tcias.co.uk>
2017-12-22 14:10:08 +0000

    Fix regex for determining end state of game

--- src/handle-game-over.js
+++ src/handle-game-over.js
@@ -1,7 +1,8 @@
 export default function (line, parserState, emit, log) {
   // Check if the game is over.
-  var gameOverRegex = /\[Power\] GameState\.DebugPrintPower\(\) - TAG_CHANGE Entity=(.*) tag=PLAYSTATE value=(LOST|WON|TIED)$/;
+  var gameOverRegex = /\[Power\] GameState\.DebugPrintPower\(\) - TAG_CHANGE Entity=(.*) tag=PLAYSTATE value=(LOST|WON|TIED)/;
   if (gameOverRegex.test(line)) {
+    log.gameOver('Check if game has ended: ' + gameOverRegex.test(line));
     var parts = gameOverRegex.exec(line);
     // Set the status for the appropriate player.
     parserState.players.forEach(function (player) {
@@ -10,6 +11,7 @@ export default function (line, parserState, emit, log) {
       }
     });
     parserState.gameOverCount++;
+    log.gameOver('Players: ' + parserState.players);
     // When both players have lost, emit a game-over event.
     if (parserState.gameOverCount === 2) {
       log.gameOver('The current game has ended.');
