c40eb9f7742cebc6e96e46a460a0cc72d67c5951
David Cox <losttime.shuffle@gmail.com>
2019-03-30 15:39:29 -0600

    Convert indentation to spaces
    Consistent with project

--- src/file-watcher.js
+++ src/file-watcher.js
@@ -1,33 +1,33 @@
 import fs from 'fs';
 
 export default class FileWatcher {
-	constructor(filePath) {
-		this.filePath = filePath;
-	}
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
 
-	start(listener) {
-		var fileSize = fs.statSync(this.filePath).size;
-		fs.watchFile(this.filePath, function (current, previous) {
-			if (current.mtime <= previous.mtime) { return; }
-	
-			// We're only going to read the portion of the file that we have not read so far.
-			var newFileSize = fs.statSync(this.filePath).size;
-			var sizeDiff = newFileSize - fileSize;
-			if (sizeDiff < 0) {
-			  fileSize = 0;
-			  sizeDiff = newFileSize;
-			}
-			var buffer = new Buffer(sizeDiff);
-			var fileDescriptor = fs.openSync(this.filePath, 'r');
-			fs.readSync(fileDescriptor, buffer, 0, sizeDiff, fileSize);
-			fs.closeSync(fileDescriptor);
-			fileSize = newFileSize;
-	  
-			listener(buffer);
-		});	
-	}
+  start(listener) {
+    var fileSize = fs.statSync(this.filePath).size;
+    fs.watchFile(this.filePath, function (current, previous) {
+      if (current.mtime <= previous.mtime) { return; }
+  
+      // We're only going to read the portion of the file that we have not read so far.
+      var newFileSize = fs.statSync(this.filePath).size;
+      var sizeDiff = newFileSize - fileSize;
+      if (sizeDiff < 0) {
+        fileSize = 0;
+        sizeDiff = newFileSize;
+      }
+      var buffer = new Buffer(sizeDiff);
+      var fileDescriptor = fs.openSync(this.filePath, 'r');
+      fs.readSync(fileDescriptor, buffer, 0, sizeDiff, fileSize);
+      fs.closeSync(fileDescriptor);
+      fileSize = newFileSize;
+    
+      listener(buffer);
+    });	
+  }
 
-	stop() {
-		fs.unwatchFile(this.filePath);
-	}
+  stop() {
+    fs.unwatchFile(this.filePath);
+  }
 }
